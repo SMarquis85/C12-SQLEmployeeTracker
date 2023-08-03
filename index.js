209316b2c21035cf785dab81eecd3527595d93a4
SMarquis85 <sophienathalie_85@outlook.com>
2023-08-03 19:25:03 -0400

    updated index.js and connection.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer");
 const figlet = require("figlet");
 const chalk = require("chalk");
-const mysql = require("mysql2");
 const cTable = require("console.table");
 
 const connection = require('./config/connection');
@@ -26,11 +25,11 @@ connection.connect((err) => {
   initialQuery();
 });
 
-function mainPrompt() {
+initialQuery = () => {
   inquirer
     .prompt({
       name: "action",
-      type: "list",
+      type: "rawlist",
       message: "What would you like to do?",
       choices: [
         "View department, roles or employees",
@@ -45,23 +44,29 @@ function mainPrompt() {
     .then((answer) => {
       switch (answer.action) {
         case "View department, roles or employees":
-          viewPrompt();
+          viewTable();
           break;
+
         case "Add department, roles or employees":
-          addPrompt();
+          addValue();
           break;
+
         case "Remove department":
-          removeDep(); // Call the removeDep function
+          removeDep();
           break;
+
         case "Update employee role":
-          updateEmpRole();
+          updateRole();
           break;
+
         case "Remove employee":
           removeEmp();
           break;
+
         case "View department budgets":
-          viewBudgets();
+          viewBudget();
           break;
+
         case "Exit":
           connection.end();
           break;
@@ -69,63 +74,10 @@ function mainPrompt() {
     });
 }
 
-// function to view tables of departments, roles and/or employees
 viewTable = () => {
-  inquirer
-    .prompt({
-      name: "view_table",
-      type: "list",
-      message: "Which table would you like to view?",
-      choices: ["Departments", "Roles", "Employees"],
-    })
-    .then((val) => {
-      if (val.view_table === "Departments") {
-        connection.query(`SELECT dept_id AS Department_ID, departments.name AS Department_Name FROM departments`, (err, res) => {
-          if (err) throw err;
-          console.log(' ');
-          console.log(chalk.green.bold(`====================================================================================`));
-          console.log(`                              ` + chalk.red.bold(`All Departments:`));
-          console.table(res);
-          console.log(chalk.green.bold(`====================================================================================`));
-          console.log(' ');
-          initialQuery();
-        });
-      } else if (val.view_table === "Roles") {
-        const query = `SELECT roles.role_id AS Role_ID, roles.title AS Title, CONCAT('$', FORMAT (salary, 0)) AS Salary, departments.name AS Department 
-        FROM roles 
-        INNER JOIN departments ON roles.dept_id = departments.dept_id 
-        ORDER BY roles.role_id ASC`
-        connection.query(query, (err, res) => {
-          if (err) throw err;
-          console.log(' ');
-          console.log(chalk.green.bold(`====================================================================================`));
-          console.log(`                              ` + chalk.red.bold(`All Roles:`));
-          console.table(res);
-          console.log(chalk.green.bold(`====================================================================================`));
-          console.log(' ');
-          initialQuery();
-        });
-      } else if (val.view_table === "Employees") {
-        const query = `SELECT emp_id AS Employee_ID, first_name AS First_Name, last_name AS Last_Name, title AS Title, CONCAT('$', FORMAT (salary, 0)) AS Salary, departments.name AS Department 
-          FROM employees 
-          INNER JOIN roles ON employees.role_Id = roles.role_id 
-          INNER JOIN departments ON roles.dept_id = departments.dept_id 
-          ORDER BY last_name ASC`
-        connection.query(query, (err, res) => {
-          if (err) throw err;
-          console.log(' ');
-          console.log(chalk.green.bold(`====================================================================================`));
-          console.log(`                              ` + chalk.red.bold(`All Employees:`));
-          console.table(res);
-          console.log(chalk.green.bold(`====================================================================================`));
-          console.log(' ');
-          initialQuery();
-        });
-      }
-    });
+  // ... (rest of the viewTable function, no changes needed)
 }
 
-// function to add a department, role and/or employee
 addValue = () => {
   inquirer
     .prompt({
@@ -169,141 +121,49 @@ addValue = () => {
     });
 }
 
-// function to update the role of a single employee
 updateRole = () => {
-
-  let listOfEmployees = [];
-  let listOfRoles = [];
-  let employeeLastName = null;
-
-  // asks the user for the last name of the employee they would like to update
-  inquirer
-    .prompt([
-      {
-        name: "empLastName",
-        type: "input",
-        message:
-          "What is the last name of the employee you would like to update?",
-      }
-    ])
-    // then it searches the database for employees with that last name and puts them into an array
-    .then((answer) => {
-
-      employeeLastName = answer.empLastName;
-      // db query to find all employees by user inputted last name
-      // then puts part of the response into an array for subsequent inquirer question
-      // then displays info to the user in table
-      const query = `SELECT emp_id AS Employee_ID, first_name AS First_Name, last_name AS Last_Name, title AS Title, salary AS Salary, departments.name AS Department FROM employees 
-      INNER JOIN roles ON employees.role_Id = roles.role_id
-      INNER JOIN departments ON roles.dept_id = departments.dept_id 
-      WHERE ?`;
-      // 
-      connection.query(query, { last_name: answer.empLastName }, (err, res) => {
-        if (err) throw err;
-
-        console.log(` `)
-        console.log(chalk.green.bold(`====================================================================================`));
-        console.log(`                              ` + chalk.red.bold(`Employee Information:`));
-        console.table(res);
-        console.log(chalk.green.bold(`====================================================================================`));
-        console.log(` `);
-
-        listOfEmployees = res.map(employee => (
-          {
-            name: employee.First_Name,
-            value: employee.Employee_ID
-          }
-        ));
-
-        // db query to find all roles and then put them into an array for a subsequent inquirer question
-        connection.query("SELECT * FROM roles", (err, res) => {
-          if (err) throw err;
-
-          listOfRoles = res.map(role => (
-            {
-              name: role.title,
-              value: role.role_id
-            }
-          ))
-
-          inquirer.prompt([
-            {
-              type: "list",
-              name: "nameConfirm",
-              message: "Please select the employee to confirm",
-              choices: listOfEmployees
-            },
-            {
-              type: "list",
-              name: "roleChoice",
-              message: "Please select a new role for the employee",
-              choices: listOfRoles
-            }
-          ])
-            .then((answers) => {
-
-              const query = `UPDATE employees SET role_id = ${answers.roleChoice} WHERE emp_id = ${answers.nameConfirm}`;
-              connection.query(query, (err, res) => {
-                if (err) throw err;
-              });
-            })
-            .then(() => {
-              const query = `SELECT emp_id AS Employee_ID, first_name AS First_Name, last_name AS Last_Name, title AS Title, salary AS Salary, departments.name AS Department FROM employees 
-                INNER JOIN roles ON employees.role_Id = roles.role_id
-                INNER JOIN departments ON roles.dept_id = departments.dept_id 
-                WHERE ?`;
-              connection.query(query, { last_name: employeeLastName }, (err, res) => {
-                if (err) throw err;
-                console.log(` `);
-                console.log(chalk.green.bold(`====================================================================================`));
-                console.log(`                              ` + chalk.red.bold(`Updated Employee Information:`));
-                console.table(res);
-                console.log(chalk.green.bold(`====================================================================================`));
-                console.log(` `);
-                initialQuery();
-              })
-            });
-        });
-      });
-    });
-
+  // ... (rest of the updateRole function, no changes needed)
 }
 
-// function to remove a department from the database
-
-function removeDep() {
+removeDep = () => {
   inquirer
     .prompt([
       {
-        name: "departmentName",
+        name: "depToRemove",
         type: "input",
         message: "What is the department you would like to remove?",
       },
     ])
     .then((answer) => {
-      const departmentName = answer.departmentName;
-
-      // Debugging statement to check the departmentName
-      console.log("Department name to remove:", departmentName);
-
-      // Check if the department exists in the database
-      const checkDepartmentQuery = "SELECT * FROM departments WHERE name = ?";
-      connection.query(checkDepartmentQuery, [departmentName], (err, results) => {
+      const query = `SELECT dep_id AS Department_ID FROM departments WHERE name = ?`;
+      connection.query(query, [answer.depToRemove], (err, res) => {
         if (err) throw err;
-
-        // Debugging statement to check the query results
-        console.log("Query results:", results);
-
-        if (results.length === 0) {
-          console.log("No department found");
-          mainPrompt();
+        if (res.length === 0) {
+          console.log(chalk.red.inverse("No department found"));
+          initialQuery();
         } else {
-          const removeDepartmentQuery = "DELETE FROM departments WHERE name = ?";
-          connection.query(removeDepartmentQuery, [departmentName], (err, result) => {
-            if (err) throw err;
-            console.log(`Department "${departmentName}" has been removed.`);
-            mainPrompt();
-          });
+          console.log(chalk.green.inverse("Department found"));
+          console.log(` `);
+          console.log(chalk.green.bold(`====================================================================================`));
+          console.log(`                              ` + chalk.red.bold(`Department Information:`));
+          console.table(res);
+          console.log(chalk.green.bold(`====================================================================================`));
+          inquirer
+            .prompt({
+              name: "depConfirm",
+              type: "number",
+              message: "Please enter the department ID to confirm choice:",
+            })
+            .then((answer) => {
+              const deleteQuery = `DELETE FROM departments WHERE dep_id = ?`;
+              connection.query(deleteQuery, [answer.depConfirm], (err, res) => {
+                if (err) throw err;
+                console.log(chalk.green.bold(`====================================================================================`));
+                console.log(`                  ` + chalk.red.bold(`Department #${answer.depConfirm} has been removed.`));
+                console.log(chalk.green.bold(`====================================================================================`));
+                initialQuery();
+              });
+            });
         }
       });
     });
